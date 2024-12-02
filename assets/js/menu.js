f937fd86d1f464ff40097b0a33083b48634c29e2
Robin Sanchez-Avila <robin.sa28@gmail.com>
2024-12-02 18:12:11 +0100

    [feature] Implement login functionality with modal; add LoginController, login form, and update navigation for authentication

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,1 +1,9 @@
-import { Modal } from "bootstrap";
+import { Modal, Dropdown } from 'bootstrap';
+
+// Initialisation de la modal si nécessaire
+document.addEventListener('DOMContentLoaded', () => {
+    const modalElement = document.getElementById('modalLogin');
+    if (modalElement) {
+        const modal = new Modal(modalElement);
+    }
+});
