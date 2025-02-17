396845cbf494baca520fb79bc4461d1773d9b9c4
Robin Sanchez-Avila <robin.sa28@gmail.com>
2025-02-17 13:19:12 +0100

    Enhance two-factor authentication email handling

--- src/Service/Mailer/AuthCodeEmailGenerator.php
+++ src/Service/Mailer/AuthCodeEmailGenerator.php
@@ -1,33 +1,54 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Service\Mailer;
 
 use danielburger1337\SchebTwoFactorBundle\Model\TwoFactorEmailInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use danielburger1337\SchebTwoFactorBundle\Mailer\AuthCodeEmailGeneratorInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
-
-class AuthCodeEmailGenerator implements AuthCodeEmailGeneratorInterface
+final class AuthCodeEmailGenerator implements AuthCodeEmailGeneratorInterface
 {
+    private Address|string|null $senderAddress;
+
+    public function __construct(
+        #[Autowire(env: 'AUTH_CODE_SUBJECT')] private readonly string $subject,
+        #[Autowire(env: 'AUTH_CODE_SENDER_EMAIL')] string|null $senderEmail,
+        #[Autowire(env: 'AUTH_CODE_SENDER_NAME')] ?string $senderName = null,
+    ) {
+        if (null !== $senderEmail && null !== $senderName) {
+            $this->senderAddress = new Address($senderEmail, $senderName);
+        } elseif (null !== $senderEmail) {
+            $this->senderAddress = $senderEmail;
+        }
+    }
+
     public function createAuthCodeEmail(TwoFactorEmailInterface $user): TemplatedEmail
     {
         $authCode = $user->getEmailAuthCode();
         if (null === $authCode) {
-            throw new \InvalidArgumentException('User must have an authentication code.');
+            throw new \InvalidArgumentException();
         }
 
-        $email = new TemplatedEmail();
-        $expiresAt = $user->getEmailAuthCodeExpiresAt()->setTimezone(new \DateTimeZone('Europe/Paris'))->format('H:i');
-
-        $email
+        $message = new TemplatedEmail();
+        $expiresAt = $user->getEmailAuthCodeExpiresAt()->setTimezone(new \DateTimeZone('Europe/Paris'))->format("H:i");
+        // TODO : Intl et timezone du mail basé sur config user
+        $message
             ->to($user->getEmailAuthRecipient())
-            ->subject('Code de vérification, valide jusqu\'à ' . $expiresAt)
+            ->subject('Votre code', ['expiresAt' => $expiresAt])
             ->htmlTemplate('security/email/authcode.html.twig')
             ->context([
                 'authCode' => $authCode,
                 'expiresAt' => $expiresAt,
-            ]);
+                'subject' => $this->subject,
+            ])
+        ;
 
-        return $email;
+        if (null !== $this->senderAddress) {
+            $message->from($this->senderAddress);
+        }
+
+        return $message;
     }
 }
