6c43803d9a180d7f629b5e71e8435e7e7f86f193
Robin Sanchez-Avila <robin.sa28@gmail.com>
2025-06-11 17:41:40 +0200

    adjusted password security check

--- src/Form/Type/PasswordConfirmType.php
+++ src/Form/Type/PasswordConfirmType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Validator\Constraints\PasswordStrength;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class PasswordConfirmType extends AbstractType
 {
@@ -32,16 +33,27 @@ class PasswordConfirmType extends AbstractType
                 'first_options' => [
                     'constraints' => [
                         new NotBlank([
-                            'message' => 'Veuillez entrer un mot de passe',
+                            'message' => 'Le mot de passe est obligatoire',
                         ]),
                         new Length([
-                            'min' => 6,
-                            'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                            'min' => 8,
+                            'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères',
                             'max' => 4096,
+                            'maxMessage' => 'Le mot de passe ne peut pas dépasser {{ limit }} caractères',
                         ]),
-                        new PasswordStrength([], 2),
-                        new NotCompromisedPassword(),
+                        new Regex([
+                            'pattern' => '/^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/',
+                            'message' => 'Le mot de passe doit contenir au minimum une majuscule, un chiffre et un caractère spécial.',
+                        ]),
+                        new PasswordStrength([
+                            'message' => 'Le mot de passe est trop faible. Utilisez des lettres majuscules et minuscules, des chiffres, et des symboles.',
+                            'minScore' => PasswordStrength::STRENGTH_WEAK,
+                        ]),
+                        new NotCompromisedPassword([
+                            'message' => 'Ce mot de passe a été compromis lors d\'une fuite de données. Veuillez en choisir un autre.',
+                        ]),
                     ],
+
                     'label' => 'Mot de passe',
                     'attr' => [
                         'placeholder' => 'Votre mot de passe',
@@ -55,7 +67,7 @@ class PasswordConfirmType extends AbstractType
                         'class' => 'form-control',
                     ],
                 ],
-                'invalid_message' => 'Les mots de passe ne correspondent pas.',
+                'invalid_message' => 'Les deux mots de passe doivent être identiques',
                 'mapped' => false,
             ]);
     }
